5f8e2b94fc14c49c6c136131eca538b75aa4f5b3
VestigeofShadows <jonlin1005@gmail.com>
2023-10-29 14:40:35 -0700

    Initial commit of project.

---
+++ Phase4Prototypes/src/CSE360Project.java
@@ -0,0 +1,17 @@
+//this is the mainline
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class CSE360Project {
+	public static void main(String[] args) {
+		//launch(args);
+		System.out.print("testing");
+	}
+	
+	StackPane root = new StackPane();
+}
